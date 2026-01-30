b6e4feaed45a23749788670dad02f7b5b5607c6b
Isaac Ruiz <isaacruizcorporan01@gmail.com>
2026-01-30 15:24:44 -0400

    feat: configuring products controller and adding the respective DTOs

--- trabajo_finall/Controllers/ProductosController.cs
+++ trabajo_finall/Controllers/ProductosController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using trabajo_finall.Models;
+using System.Threading.Tasks;
+using trabajo_finall.Dtos;
+using trabajo_finall.Data;
 
 namespace trabajo_finall.Controllers
 {
@@ -6,28 +11,68 @@ namespace trabajo_finall.Controllers
     [Route("productos")]
     public class ProductosController : ControllerBase
     {
+        private readonly AppDbContext dbContext;
+
+        public ProductosController(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         [HttpPost("añadir")]
-        public IActionResult Añadir()
+        public async Task<IActionResult> Añadir([FromBody] ProductoCreateDto dto)
         {
-            return Ok("Producto añadido");
-        }
+            var producto = new Producto
+            {
+              nombre = dto.nombre,
+              precio = dto.precio,
+              stock = dto.stock,
+            };
 
-        [HttpDelete("eliminar")]
-        public IActionResult Eliminar(int id)
-        {
-            return Ok($"Producto {id} eliminado");
-        }
+            dbContext.Productos.Add(producto);
+            await dbContext.SaveChangesAsync();
 
-        [HttpPut("editar")]
-        public IActionResult Editar(int id)
-        {
-            return Ok($"Producto {id} editado");
+            return Ok(producto);
         }
 
         [HttpGet("detalles")]
-        public IActionResult Detalles(int id)
+        public async Task<IActionResult> Detalles(int id)
         {
-            return Ok($"Detalles del producto {id}");
+            var producto = await dbContext.Productos.FindAsync(id);
+            if (producto == null)
+                return NotFound($"Producto {id} no existe");
+
+            return Ok(producto);
+        }
+
+        [HttpPut("editar")]
+        public async Task<IActionResult> Editar(int id, [FromBody] ProductoUpdateDto dto)
+        {
+            var producto = await dbContext.Productos.FindAsync(id);
+
+            if (producto == null)
+                return NotFound($"Producto {id} no existe");
+
+            producto.nombre = dto.nombre;
+            producto.precio = dto.precio;
+            producto.stock = dto.stock;
+
+            await dbContext.SaveChangesAsync();
+
+            return Ok(producto);
+        }
+
+        [HttpDelete("eliminar")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            var producto = await dbContext.Productos.FindAsync(id);
+
+            if (producto == null)
+                return NotFound($"Producto {id} no existe");
+
+            dbContext.Productos.Remove(producto);
+            await dbContext.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
---
+++ trabajo_finall/Dtos/ProductoUpdateDto.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace trabajo_finall.Dtos
+{
+    public class ProductoUpdateDto
+    {
+        [Key]
+        public int id { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string nombre { get; set; }
+
+        [Required]
+        public decimal precio { get; set; }
+
+        [Required]
+        public int stock{ get; set; } 
+    }
+}
